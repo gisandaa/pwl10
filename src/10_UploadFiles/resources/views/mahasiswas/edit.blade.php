e777d987c5ba8030b297b7c9551e5a846d97914f
Genadi Dharma <genadislawa@gmail.com>
2021-05-04 09:59:57 +0800

    Perbaiki blade edit yang belum ada enctype dan salah path gambarnya

--- src/10_UploadFiles/resources/views/mahasiswas/edit.blade.php
+++ src/10_UploadFiles/resources/views/mahasiswas/edit.blade.php
@@ -17,38 +17,38 @@
                     </ul>
                 </div>
                 @endif
-                <form method="post" action="{{ route('mahasiswas.update', $mahasiswas->Nim) }}" id="myForm">
+                <form method="post" action="{{ route('mahasiswas.update', $mahasiswas->Nim) }}" id="myForm" enctype="multipart/form-data">
                     @csrf
                     @method('PUT') <div class="form-group">
                         <label for="Nim">Nim</label>
-                
+
                         <input type="text" name="Nim" class="form-control" id="Nim" value="{{ $mahasiswas->Nim }}" aria-describedby="Nim">
                     </div>
                     <div class="form-group">
                         <label for="Nama">Nama</label>
-                       
+
                         <input type="text" name="Nama" class="form-control" id="Nama" value="{{ $mahasiswas->Nama }}" aria-describedby="Nama">
                     </div>
                     <div class="form-group">
-                    <label for="image">Image </label>
-                    <input type="file" class="form-control" name="image" value="{{ $mahasiswas->image}}" aria-describedby="Image">
-                    <img width="100px" src="{{asset('storage/'.$mahasiswas->image)}}">
+                        <label for="image">Image </label>
+                        <input type="file" class="form-control" name="image" value="{{ $mahasiswas->image}}" aria-describedby="Image">
+                        <img width="100px" src="{{asset('storage/'. $mahasiswas->foto)}}">
                     </div>
                     <div class="form-group">
                         <label for="Kelas">Kelas</label>
-                       <select name ="Kelas" class="form-control">
-                       @foreach($kelas as $kls)
-                       <option value="{{$kls->id}}"{{ $mahasiswas->kelas_id == $kls->id ? 'selected' : ''}}>{{$kls->nama_kelas}}</option>
-                        @endforeach
+                        <select name="Kelas" class="form-control">
+                            @foreach($kelas as $kls)
+                            <option value="{{$kls->id}}" {{ $mahasiswas->kelas_id == $kls->id ? 'selected' : ''}}>{{$kls->nama_kelas}}</option>
+                            @endforeach
                         </select>
                     </div>
                     <div class="form-group">
                         <label for="Jurusan">Jurusan</label>
-                      
+
                         <input type="Jurusan" name="Jurusan" class="form-control" id="Jurusan" value="{{ $mahasiswas->Jurusan }}" aria-describedby="Jurusan">
                     </div>
-                    
-        
+
+
                     <button type="submit" class="btn btn-primary">Submit</button>
                 </form>
             </div>
